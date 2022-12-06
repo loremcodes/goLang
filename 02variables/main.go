cad36c43e917c4962be5537e8200e0ef7d5e6afa
Prasad Junghare <86673407+loremcodes@users.noreply.github.com>
2022-12-06 18:46:43 +0530

    variables learnt

--- 02variables/main.go
+++ 02variables/main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello variables")
+	// create variable -> string
+	var username string = "prasad"
+	// Print the variable
+	fmt.Println(username)
+	// find the type of variable
+	fmt.Printf("The username variable is of type: %T \n", username)
+
+	//    variable -> bool
+	var isTrue bool = true
+    fmt.Println(isTrue)
+    fmt.Printf("The isTrue variable is of type: %T", isTrue)
 }
