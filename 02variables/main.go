14b0370f4a1445d739f10a307a4342cbe2c97503
Prasad Junghare <86673407+loremcodes@users.noreply.github.com>
2022-12-06 19:10:08 +0530

    variable finished

--- 02variables/main.go
+++ 02variables/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const UserId int = 1967971242007 //  Public variable because 'U' is captial
+
 func main() {
 	// create variable -> string
 	var username string = "prasad"
@@ -10,8 +12,24 @@ func main() {
 	// find the type of variable
 	fmt.Printf("The username variable is of type: %T \n", username)
 
-	//    variable -> bool
+	// variable -> bool
 	var isTrue bool = true
-    fmt.Println(isTrue)
-    fmt.Printf("The isTrue variable is of type: %T", isTrue)
+	fmt.Println(isTrue)
+	fmt.Printf("The isTrue variable is of type: %T \n", isTrue)
+
+	// default values for string
+    var emptyInt int
+    fmt.Println(emptyInt)
+
+    // Implicit type
+    var variableWithoutType = 34
+    fmt.Println(variableWithoutType)
+
+    // no var style -> only avaibale in main function
+    variableWithoutVarKey := 4307
+    fmt.Println(variableWithoutVarKey)
+
+    // Calling public variable
+    fmt.Println(UserId)
+    fmt.Printf("UserId is type of: %T \n ", UserId)
 }
