d7d9db0fd244c83ef744fbb9c7246c8160d8110b
Prasad Junghare <86673407+loremcodes@users.noreply.github.com>
2022-12-06 19:41:42 +0530

    04 added

--- 04conversion/main.go
+++ 04conversion/main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"bufio"
-	"os"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Input your rating between 1 to 5")
 	input, _ := reader.ReadString('\n')
-	fmt.Println(input)
+	// fmt.Print(input)
+
+	// adding 1 to the initial input
+	addToInput, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil{
+		fmt.Println(err)
+	} else {
+		fmt.Println(addToInput + 1, " is your final rating Thanks..!")
+	}
 }
